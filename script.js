917b01c3166e4c4f7ab4285639834e6d5b704b6a
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-07 08:43:58 -0500

    Searching for another solution

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 
-const grid = document.createElement('div')
-grid.setAttribute('class','myClass');
-document.body.appendChild(grid);
+// const grid = document.createElement('div')
+// grid.setAttribute('class','myClass');
+// document.body.appendChild(grid);
 
 
 
-let rows = document.getElementById('gridRow');
-let cells = document.getElementById('cell');
+// let rows = document.getElementById('gridRow');
+// let cells = document.getElementById('cell');
 
 
 
 
-function defaultGrid(){
-	makeRows(16);
-	makeColumns(16);
-}
+// function defaultGrid(){
+// 	makeRows(16);
+// 	makeColumns(16);
+// }
 
-function makeRows(rowNum) {
-	for (r = 0; r < rowNum; r++){
-		let row = document.createElement('div');
-		myClass.appendChild(row).className ='gridRow';
-	}
-}
+// function makeRows(rowNum) {
+// 	for (r = 0; r < rowNum; r++){
+// 		let row = document.createElement('div');
+// 		myClass.appendChild(row).className ='gridRow';
+// 	}
+// }
 
-function makeColumns(cellNum){
-	for (i = 0; i < rows.length; i++){
-		for(j = 0; j < cellNum; j++ );{
-	let column = document.createElement('div');
-	rows[j].appendChild(column).className='cell';
-	};
-};
-}
+// function makeColumns(cellNum){
+// 	for (i = 0; i < rows.length; i++){
+// 		for(j = 0; j < cellNum; j++ );{
+// 	let column = document.createElement('div');
+// 	rows[j].appendChild(column).className='cell';
+// 	};
+// };
+// }
+
+
+
