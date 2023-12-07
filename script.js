c2855ebcddc0e1392be2822e0ea05742c8a9a82d
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-07 08:39:46 -0500

    Creating a 16x16 grid.

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const grid = document.createElement('div')
+grid.setAttribute('class','myClass');
+document.body.appendChild(grid);
+
+
+
+let rows = document.getElementById('gridRow');
+let cells = document.getElementById('cell');
+
+
+
+
+function defaultGrid(){
+	makeRows(16);
+	makeColumns(16);
+}
+
+function makeRows(rowNum) {
+	for (r = 0; r < rowNum; r++){
+		let row = document.createElement('div');
+		myClass.appendChild(row).className ='gridRow';
+	}
+}
+
+function makeColumns(cellNum){
+	for (i = 0; i < rows.length; i++){
+		for(j = 0; j < cellNum; j++ );{
+	let column = document.createElement('div');
+	rows[j].appendChild(column).className='cell';
+	};
+};
+}
