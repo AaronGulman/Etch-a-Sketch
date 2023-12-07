2972a00e3998c9d85bfb396aeac3d492a65b169e
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-07 11:00:41 -0500

    Deleted unnecessary comments.

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-
-// const grid = document.createElement('div')
-// grid.setAttribute('class','myClass');
-// document.body.appendChild(grid);
-
-
-
-// let rows = document.getElementById('gridRow');
-// let cells = document.getElementById('cell');
-
-
-
-
-// function defaultGrid(){
-// 	makeRows(16);
-// 	makeColumns(16);
-// }
-
-// function makeRows(rowNum) {
-// 	for (r = 0; r < rowNum; r++){
-// 		let row = document.createElement('div');
-// 		myClass.appendChild(row).className ='gridRow';
-// 	}
-// }
-
-// function makeColumns(cellNum){
-// 	for (i = 0; i < rows.length; i++){
-// 		for(j = 0; j < cellNum; j++ );{
-// 	let column = document.createElement('div');
-// 	rows[j].appendChild(column).className='cell';
-// 	};
-// };
-// }
-
 const body = document.querySelector('body');
 body.style.background = 'linear-gradient(to left, #fc466b, #3f5efb)'
 
